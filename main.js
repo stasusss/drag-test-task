e8b71c9e9c01207761251c0ce2e945dec81ce431
Stanislav <stasusss07@gmail.com>
2020-09-21 14:12:07 +0300

    style

--- main.js
+++ main.js
@@ -13,14 +13,6 @@ ball.onmousedown = function(event) {
   let shiftX = event.clientX - ball.getBoundingClientRect().left;
   let shiftY = event.clientY - ball.getBoundingClientRect().top;
 
-  ball.style.position = 'absolute';
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
-
-  moveAt(event.pageX, event.pageY);
-
-  // переносит мяч на координаты (pageX, pageY),
-  // дополнительно учитывая изначальный сдвиг относительно указателя мыши
   function moveAt(pageX, pageY) {
 
     if (pageX > limits.right) {
@@ -43,13 +35,15 @@ ball.onmousedown = function(event) {
     moveAt(event.pageX, event.pageY);
   }
 
-  // передвигаем мяч при событии mousemove
   document.addEventListener('mousemove', onMouseMove);
 
-  // отпустить мяч, удалить ненужные обработчики
   ball.onmouseup = function() {
     document.removeEventListener('mousemove', onMouseMove);
     ball.onmouseup = null;
   };
 
+};
+
+ball.ondragstart = function() {
+  return false;
 };
